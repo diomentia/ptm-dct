ec0fab0161fbda24dd9bb59f4ac54035da9fd2f3
Demetrius <thedioxider@mail.ru>
2024-07-30 18:15:45 +0300

    fix(rfid): get new version of SDK

--- app/src/main/java/space/diomentia/mcmcontroller/InitialActivity.kt
+++ app/src/main/java/space/diomentia/mcmcontroller/InitialActivity.kt
@@ -6,13 +6,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -20,12 +18,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.ubx.usdk.USDKManager
 import com.ubx.usdk.rfid.aidl.IRfidCallback
-import com.ubx.usdk.rfid.aidl.ITag6BCallback
 import com.ubx.usdk.util.QueryMode
 import space.diomentia.mcmcontroller.ui.theme.MCMControllerTheme
 
@@ -60,18 +55,19 @@ fun RfidInfo(modifier: Modifier = Modifier) {
         }
         Text("Device ID: $deviceId")
         var rfidStatus by remember { mutableStateOf<String>(null.toString()) }
-        var rfidFound by remember { mutableStateOf<String>(null.toString()) }
-        var rfidMemory by remember { mutableStateOf<String>(null.toString()) }
+        var rfidEpc by remember { mutableStateOf<String>(null.toString()) }
+        var rfidTid by remember { mutableStateOf<String>(null.toString()) }
+        var rfidData by remember { mutableStateOf<String>(null.toString()) }
         var rfidRssi by remember { mutableStateOf<String>(null.toString()) }
-        val context = LocalContext.current
-        USDKManager.getInstance().init(context) { status: USDKManager.STATUS ->
+        USDKManager.getInstance().init() { status ->
             val rfidManager = USDKManager.getInstance().rfidManager
-            rfidStatus = "${status.name} at ${rfidManager.outputPower}"
+            rfidStatus = "${if (status) "SUCCESS" else "FAIL"} at ${rfidManager.outputPower}"
             rfidManager.queryMode = QueryMode.EPC_TID
             rfidManager.registerCallback(object : IRfidCallback {
-                override fun onInventoryTag(epc: String?, mem: String?, rssi: String?) {
-                    rfidFound = epc.toString()
-                    rfidMemory = mem.toString()
+                override fun onInventoryTag(epc: String?, data: String?, rssi: String?) {
+                    rfidEpc = epc.toString()
+                    rfidTid = data.toString()
+                    rfidData = rfidManager.inventoryParameter.MaskData.toString()
                     rfidRssi = rssi.toString()
                 }
 
@@ -80,8 +76,9 @@ fun RfidInfo(modifier: Modifier = Modifier) {
             rfidManager.startRead()
         }
         Text("RFID manager: $rfidStatus")
-        Text("RFID found: $rfidFound")
-        Text("RFID memory: $rfidMemory")
+        Text("RFID epc: $rfidEpc")
+        Text("RFID tid: $rfidTid")
+        Text("RFID data: $rfidData")
         Text("RFID rssi: $rfidRssi")
         /*
         TextField(value = rfidMemory, onValueChange = {
