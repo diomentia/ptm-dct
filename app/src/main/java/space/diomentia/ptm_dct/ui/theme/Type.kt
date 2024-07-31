a6f15f4d2ecfa8a185a97c8f5519ee726ad07da6
Demetrius <thedioxider@mail.ru>
2024-07-31 03:18:31 +0300

    feat(theme): add Montserrat font

--- app/src/main/java/space/diomentia/ptm_dct/ui/theme/Type.kt
+++ app/src/main/java/space/diomentia/ptm_dct/ui/theme/Type.kt
@@ -1,34 +1,134 @@
 package space.diomentia.ptm_dct.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontVariation
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import space.diomentia.ptm_dct.R
+
+@OptIn(ExperimentalTextApi::class)
+val MontserratFF = FontFamily(
+    Font(
+        R.font.montserrat,
+        weight = FontWeight.W100,
+        variationSettings = FontVariation.Settings(FontVariation.weight(100))
+    ),
+    Font(
+        R.font.montserrat,
+        weight = FontWeight.W200,
+        variationSettings = FontVariation.Settings(FontVariation.weight(200))
+    ),
+    Font(
+        R.font.montserrat,
+        weight = FontWeight.W300,
+        variationSettings = FontVariation.Settings(FontVariation.weight(300))
+    ),
+    Font(
+        R.font.montserrat,
+        weight = FontWeight.W400,
+        variationSettings = FontVariation.Settings(FontVariation.weight(400))
+    ),
+    Font(
+        R.font.montserrat,
+        weight = FontWeight.W500,
+        variationSettings = FontVariation.Settings(FontVariation.weight(500))
+    ),
+    Font(
+        R.font.montserrat,
+        weight = FontWeight.W600,
+        variationSettings = FontVariation.Settings(FontVariation.weight(600))
+    ),
+    Font(
+        R.font.montserrat,
+        weight = FontWeight.W700,
+        variationSettings = FontVariation.Settings(FontVariation.weight(700))
+    ),
+    Font(
+        R.font.montserrat,
+        weight = FontWeight.W800,
+        variationSettings = FontVariation.Settings(FontVariation.weight(800))
+    ),
+    Font(
+        R.font.montserrat,
+        weight = FontWeight.W900,
+        variationSettings = FontVariation.Settings(FontVariation.weight(900))
+    )
+)
+
+@OptIn(ExperimentalTextApi::class)
+val MontserratItalicFF = FontFamily(
+    Font(
+        R.font.montserrat_italic,
+        weight = FontWeight.W100,
+        variationSettings = FontVariation.Settings(FontVariation.weight(100))
+    ),
+    Font(
+        R.font.montserrat_italic,
+        weight = FontWeight.W200,
+        variationSettings = FontVariation.Settings(FontVariation.weight(200))
+    ),
+    Font(
+        R.font.montserrat_italic,
+        weight = FontWeight.W300,
+        variationSettings = FontVariation.Settings(FontVariation.weight(300))
+    ),
+    Font(
+        R.font.montserrat_italic,
+        weight = FontWeight.W400,
+        variationSettings = FontVariation.Settings(FontVariation.weight(400))
+    ),
+    Font(
+        R.font.montserrat_italic,
+        weight = FontWeight.W500,
+        variationSettings = FontVariation.Settings(FontVariation.weight(500))
+    ),
+    Font(
+        R.font.montserrat_italic,
+        weight = FontWeight.W600,
+        variationSettings = FontVariation.Settings(FontVariation.weight(600))
+    ),
+    Font(
+        R.font.montserrat_italic,
+        weight = FontWeight.W700,
+        variationSettings = FontVariation.Settings(FontVariation.weight(700))
+    ),
+    Font(
+        R.font.montserrat_italic,
+        weight = FontWeight.W800,
+        variationSettings = FontVariation.Settings(FontVariation.weight(800))
+    ),
+    Font(
+        R.font.montserrat_italic,
+        weight = FontWeight.W900,
+        variationSettings = FontVariation.Settings(FontVariation.weight(900))
+    )
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = MontserratFF,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
+        fontFamily = MontserratFF,
+        fontWeight = FontWeight.ExtraBold,
+        fontSize = 32.sp,
+        lineHeight = 38.sp,
         letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = MontserratFF,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
 )
