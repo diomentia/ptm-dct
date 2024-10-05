2c2161637f240b387a13a8d4686105a176334e09
Demetrius <thedioxider@mail.ru>
2024-10-05 14:02:30 +0300

    feat(measurements): journal is scrollable

--- app/src/main/java/space/diomentia/ptm_dct/MeasurementsActivity.kt
+++ app/src/main/java/space/diomentia/ptm_dct/MeasurementsActivity.kt
@@ -14,7 +14,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Battery0Bar
 import androidx.compose.material.icons.filled.Battery2Bar
@@ -148,6 +150,7 @@ private fun Contents() {
     }
     Column(
         modifier = Modifier
+            .verticalScroll(rememberScrollState())
             .fillMaxSize()
             .padding(horizontal = 16.dp, vertical = 16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
